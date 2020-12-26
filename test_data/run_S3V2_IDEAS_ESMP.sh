2ee6c184ce3362900861a386d32374e77777b827
Universe <universe@Guanjues-MacBook-Pro.local>
2020-12-26 18:25:43 -0500

    try docker

--- test_data/run_S3V2_IDEAS_ESMP.sh
+++ test_data/run_S3V2_IDEAS_ESMP.sh
@@ -1,19 +1,19 @@
 ### required inputs
 ###### the absolute path to the bin folder
-script_dir='/storage/home/gzx103/scratch/test_S3V2/S3V2_IDEAS_ESMP/bin/'
+script_dir='/opt/S3V2_IDEAS_ESMP/bin/'
 ###### your output folder
-output_dir='/storage/home/gzx103/scratch/test_S3V2/S3V2_IDEAS_ESMP/test_data/outputs/'
+output_dir='/opt/S3V2_IDEAS_ESMP/test_data/outputs/'
 ###### the absolute path to the your modified "metadata.for_master_peak_calls.txt" file
-metadata='/storage/home/gzx103/scratch/test_S3V2/S3V2_IDEAS_ESMP/test_data/metadata.forEScall.txt'
+metadata='/opt/S3V2_IDEAS_ESMP/test_data/metadata.forEScall.txt'
 ###### The output name
 id_name='test_S3V2_IDEAS_pipeline'
 
 ###### genome
 GENOME='mm10'
 ###### genome size (can be found in the "S3V2_IDEAS_ESMP/genomesize/" folder)
-GENOMESIZES='/storage/home/gzx103/scratch/test_S3V2/S3V2_IDEAS_ESMP/genomesize/mm10.chrom.chr11.fortest.sizes'
+GENOMESIZES='/opt/S3V2_IDEAS_ESMP/genomesize/mm10.chrom.chr11.fortest.sizes'
 ###### blacklist (can be found in the "S3V2_IDEAS_ESMP/blacklist/" folder)
-BLACK='/storage/home/gzx103/scratch/test_S3V2/S3V2_IDEAS_ESMP/blacklist/mm10-blacklist.v2.bed'
+BLACK='/opt/S3V2_IDEAS_ESMP/blacklist/mm10-blacklist.v2.bed'
 
 ###### number of threads in system
 ###### When the number of threads is too large, the multi-threads in python may fail. So it is more stable to keep it below 4. 
