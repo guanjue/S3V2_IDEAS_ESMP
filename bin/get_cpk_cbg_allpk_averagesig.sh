5f73bca03c352b72403b427158941d9864c3b145
Guanjue Xiang <gxiang@dsgxiangmacport.local>
2021-03-12 12:04:19 -0500

    opt para

--- bin/get_cpk_cbg_allpk_averagesig.sh
+++ bin/get_cpk_cbg_allpk_averagesig.sh
@@ -6,7 +6,7 @@ cat $meta_data | awk -F '\t' -v OFS='\t' -v used_mk=$mk '{if ($2==used_mk) print
 cat $meta_data | awk -F '\t' -v OFS='\t' -v used_mk=$mk '{if ($2==used_mk) print $1"."$2"."$3".ctrl.idsort.bedgraph"}' > $mk'.file_list_tmp2'
 paste $mk'.file_list_tmp1' $mk'.file_list_tmp2' > $mk'.file_list.txt'
 ### get cpk cbg
-time Rscript $script_dir1/get_common_pk_p.R $mk'.file_list.txt' $mk'_commonpkfdr01_z' 0.1 z 0.9 1.0
+time Rscript $script_dir1/get_common_pk_p.R $mk'.file_list.txt' $mk'_commonpkfdr01_z' 0.1 z 0.95 1.0
 ### get allpk
 cat $mk'_commonpkfdr01_z.cbg.txt' | awk '{if ($1!=0) print 0; else print 1}' > $mk'_commonpkfdr01_z.allpk.txt'
 ### get average signal
