546be106cc2f169e9423fb2f48252af04d18c2b6
Universe <universe@Guanjues-MacBook-Pro.local>
2022-03-10 00:01:28 -0500

    conda env settings

--- INSTALL_conda_ENV.sh
+++ INSTALL_conda_ENV.sh
@@ -3,12 +3,10 @@ conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 conda config --add channels mvdbeek
-conda config --add channels conda-forge
 
 conda update conda
 
 conda create -n s3v2_ideas gsl=2.2.1 python=3 r=3.6 bedtools ucsc_tools numpy scipy multiprocess r-doParallel r-foreach r-data.table
 
 conda activate s3v2_ideas
-
 
